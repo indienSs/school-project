4c91a225fdee8cce53d1be972b551aace5188408
Oleg Andrianov <oleg.andrianov@bk.ru>
2022-12-02 13:14:29 +0800

    added form output

--- src/App.js
+++ src/App.js
@@ -2,22 +2,27 @@ import React, {useState} from "react";
 import Header from "./components/Header";
 import Form from "./components/Form";
 import FormAdd from "./components/FormAdd";
+import FormOutput from "./components/FormOutput"
 
 function App() {
   
   const [formActive, setFormActive] = useState(false)
   
   function showForm() {
-    setFormActive(true)
+    setFormActive(prevActive => !prevActive)
   }
   
   return (
     <div className="app">
       <Header showForm={showForm}/>
-      <div className="main-content">
-        <FormAdd />
-        {formActive && <Form />}
-      </div>
+        {formActive ?
+          <div className="main-content">
+            <FormAdd />
+            <Form />
+          </div> : 
+          <FormAdd className="main-content" />
+        }
+      <FormOutput />
     </div>
   );
 }
--- src/components/FormAdd.js
+++ src/components/FormAdd.js
@@ -1,19 +1,20 @@
 import React from "react";
 
-export default function FormAdd() {
+export default function FormAdd({className}) {
     
     function searchUsers(event) {
         event.preventDefault();
     }
 
     return (
-        <form className="form-add">
+        <form className="form-add {className}">
             <p>Добавить ученика</p>
             <input type="text" placeholder="Фамилия" />
             <input type="text" placeholder="Имя" />
             <input type="text" placeholder="Номер класса" />
             <input type="text" placeholder="Буква" />
             <button onClick={searchUsers} className="search">Добавить</button>
+            
         </form>
     )
 };
